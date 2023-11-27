50ca5e1c9b3c2d76d72b381a5161686dcffa559b
Tekano Khambane <tkhambane@gmail.com>
2023-11-27 07:49:00 +0200

    feat: complete skills component

--- frontend/src/components/modal/skills/AddSkillButton.vue
+++ frontend/src/components/modal/skills/AddSkillButton.vue
@@ -1,3 +1,21 @@
 <template>
-    <h1>Add Skill</h1>
+    <button type="button" @click="$emit('add-skill')" class="addSkillButton">+ Add New Skill</button>
 </template>
+
+<style scoped>
+.addSkillButton {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 30px;
+    padding: 20px 20px;
+    margin-right: 30px;
+    border: 1px solid rgb(25, 3, 46);
+    border-radius: 10px;
+    color: white;
+    background-color: rgb(179, 176, 176);
+    outline: none;
+    cursor: pointer;
+}
+</style>
--- frontend/src/components/modal/skills/RemoveSkillButton.vue
+++ frontend/src/components/modal/skills/RemoveSkillButton.vue
@@ -1,3 +1,22 @@
 <template>
-    <h1>Remove Skill</h1>
+    <button type="button" class="removeSkillButton">
+        <i class="pi pi-trash" style="font-size: 1rem, color: white"></i>
+    </button>
 </template>
+
+<style scoped>
+.removeSkillButton {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 10px;
+    height: 30px;
+    padding: 10px 10px;
+    /* margin-right: 30px; */
+    border: 1px solid rgb(25, 3, 46);
+    border-radius: 10px;
+
+    outline: none;
+    cursor: pointer;
+}
+</style>
