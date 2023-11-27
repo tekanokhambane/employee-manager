481fda7647e5d680cf809dea5369adbb2035844e
Tekano Khambane <tkhambane@gmail.com>
2023-11-27 07:31:01 +0200

    feat: Enable employees search query

--- frontend/src/components/header/SearchInput.vue
+++ frontend/src/components/header/SearchInput.vue
@@ -1,8 +1,24 @@
 <template>
-    <input class="searchFilter" type="text" placeholder="Search..." />
+    <input class="searchFilter" type="text" placeholder="Search..." @input="handleSearchInput" />
 </template>
 
+<script setup>
+import { useEmployeesStore } from '@/stores/employeesStore';
 
+const employeesStore = useEmployeesStore();
+
+// Method to handle search input
+const handleSearchInput = (event) => {
+    const searchQuery = event.target.value.trim(); // Get the search query from the input
+    if (searchQuery.length > 0) {
+        // Perform API call with the search query
+        employeesStore.searchEmployees(searchQuery);
+    } else {
+        // If search query is empty, reset the search results
+        employeesStore.fetchEmployees();
+    }
+}
+</script>
 
 <style scoped>
 .searchFilter {
