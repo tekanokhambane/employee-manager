ad718e72e46d0207270dcd6eee9a9d94b616c98b
Tekano Khambane <tkhambane@gmail.com>
2023-11-27 06:40:21 +0200

    Enable modal toggle button and skills filter

--- frontend/src/components/header/HeaderItem.vue
+++ frontend/src/components/header/HeaderItem.vue
@@ -4,22 +4,71 @@
 
             <div class="title">
                 <h1>Employees</h1>
-                <p>There are 4 employees</p>
+                <p>There are {{ employeesStore.employees.length }} employees</p>
             </div>
             <SearchInput />
             <SelectInput />
-            <ButtonItem />
+            <ButtonItem @click="modalStore.openModalForCreate(employeesStore)" />
         </div>
+        <div class="filterItems">
+            <!-- if the employees.SelectedFilter is not null, show the selected filter template -->
+            <div v-if="employeesStore.selectedFilter">
+                <div class="selectedFilter">
+                    <div v-if="employeesStore.selectedFilter === 'date_of_birth'">
+                        <p>Filter by Date of Birth</p>
+                        <input type="date" />
+                    </div>
+                    <div v-if="employeesStore.selectedFilter === 'skills'">
+                        <p>Select Skill</p>
+                        <div class="filterBySkills">
+                            <v-select v-model="selectedSkill" label="name" :options="employeesStore.skills"></v-select>
+                        </div>
 
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </header>
 </template>
 
 <script setup>
-
+import { useModalStore } from '../../stores/modalStore.js';
+import { useEmployeesStore } from '@/stores/employeesStore';
 import SearchInput from './SearchInput.vue';
 import SelectInput from './SelectInput.vue'
 import ButtonItem from './ButtonItem.vue'
+
+
+import 'vue-select/dist/vue-select.css';
+import { computed } from 'vue';
+import axios from 'axios';
+const modalStore = useModalStore();
+
+const employeesStore = useEmployeesStore();
+
+// check if the selected filter is skills, if yes, show the skills dropdown and fetch the skills using the fetchSkills method in employeesStore
+// else if the selected filter is date_of_birth, show the date of birth input
+
+
+const selectedSkill = computed({
+    get() {
+        return employeesStore.selectedSkill
+    },
+    set(value) {
+        employeesStore.selectedSkill = value
+        if (value) {
+            console.log(value)
+            // filter employees by selected skill
+            axios.get(`/api/employees/?skills=${value.name}`).then(response => {
+                employeesStore.employees = response.data
+            })
+        } else {
+            employeesStore.fetchEmployees()
+        }
+    }
+})
+
 
 
 </script>
