38b2fbf66c9c0cc1a02f162e185415576cb6c819
Tekano Khambane <tkhambane@gmail.com>
2024-02-24 16:49:11 +0200

    create and style filter options

--- frontend/src/components/header/SelectInput.vue
+++ frontend/src/components/header/SelectInput.vue
@@ -1,3 +1,45 @@
 <template>
-    <h1>Select</h1>
+    <select name="filter">
+        <option value="">Filter by</option>
+        <option value="date_of_birth">Date of birth</option>
+        <option value="skills">Skills</option>
+    </select>
 </template>
+
+
+
+<style scoped>
+/* the select input should be transparent and the gap between the dropdown and text should be 0 */
+select {
+    background-color: transparent;
+    border: none;
+    color: white;
+    gap: 0;
+    max-width: 80px;
+    outline: none;
+}
+
+select option {
+    max-width: 80px;
+    color: black;
+    background-color: rgb(185, 185, 185);
+    border: none;
+    outline: none;
+    padding: 2rem 1.5rem;
+}
+
+select option:hover {
+    background-color: rgb(43, 7, 77);
+    color: white;
+}
+
+select option:checked {
+    background-color: rgb(43, 7, 77);
+    color: white;
+}
+
+select option:checked:hover {
+    background-color: rgb(43, 7, 77);
+    color: white;
+}
+</style>
