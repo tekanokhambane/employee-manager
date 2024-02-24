0ca3f565fe8bf1845e4ab01d0f3353b75690189a
Tekano Khambane <tkhambane@gmail.com>
2024-02-24 16:30:42 +0200

    style search input

--- frontend/src/components/header/SearchInput.vue
+++ frontend/src/components/header/SearchInput.vue
@@ -1,3 +1,28 @@
 <template>
-    <h1>Search</h1>
+    <input class="searchFilter" type="text" placeholder="Search..." />
 </template>
+
+
+
+<style scoped>
+.searchFilter {
+    width: 300px;
+    height: 30px;
+    padding: 10px;
+    border: 1px solid rgb(179, 176, 176);
+    border-radius: 5px;
+    color: white;
+    background-color: rgb(43, 7, 77);
+    outline: none;
+}
+
+.searchFilter:focus {
+    border: 1px solid white;
+    background-color: rgb(43, 7, 77);
+    color: white;
+}
+
+.searchFilter::placeholder {
+    color: white;
+}
+</style>
