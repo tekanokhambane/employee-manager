a4b29bddc6f5e435c48904e8169784dde828a408
Tekano Khambane <tkhambane@gmail.com>
2024-02-24 15:47:24 +0200

    feat: Create new user button

--- frontend/src/components/header/ButtonItem.vue
+++ frontend/src/components/header/ButtonItem.vue
@@ -1,3 +1,20 @@
 <template>
-    <h1>Button</h1>
+    <button type="button" class="button" @click="$emit('click')">New Employee</button>
 </template>
+
+
+<style scoped>
+.button {
+    background-color: rgb(43, 7, 77);
+    border: none;
+    border-radius: 50px;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+</style>
